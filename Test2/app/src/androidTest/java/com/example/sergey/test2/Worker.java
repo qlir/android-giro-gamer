4713bd574ac32fdc32d1c139411ab20395ed0507
Sergey Vlasov <sergey.vlasov@akvelon.com>
2016-11-25 13:07:09 +0300

    Improve Tacking screenshots.

--- Test2/app/src/androidTest/java/com/example/sergey/test2/Worker.java
+++ Test2/app/src/androidTest/java/com/example/sergey/test2/Worker.java
@@ -5,6 +5,7 @@ import android.app.UiAutomation;
 import android.graphics.Bitmap;
 import android.support.test.uiautomator.UiDevice;
 import android.util.Log;
+import android.view.accessibility.AccessibilityNodeInfo;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,6 +22,7 @@ public class Worker {
     // private final ActivityManager mActivityManager;
     //  private final Activity mActivity;
     public final List<Bitmap> mBitmaps;
+    private final List<Bitmap> mNeededBitmaps;
     private final UiDevice mUiDevice;
     private UiAutomation mUiAutomation;
 
@@ -28,6 +30,7 @@ public class Worker {
         mUiDevice = uiDevice;
         //mActivityManager = (ActivityManager)inst.getContext().getSystemService(Context.ACTIVITY_SERVICE);
         mBitmaps = new ArrayList<>();
+        mNeededBitmaps = new ArrayList<>();
         //mBitmaps.add(Bitmap.createBitmap(mView.getWidth(), mView.getHeight(), Bitmap.Config.ARGB_8888));
         //final File f = new File("screenshot.png");
         mUiAutomation = inst.getUiAutomation();
@@ -40,11 +43,15 @@ public class Worker {
             public void run() {
                 while (true) {
                     long start = System.currentTimeMillis();
-                    Log.d("test", "TakeScreenshot" + start);
+                    Log.d("test", "TakeScreenshot:" + start);
                     takeScreenshot();
                     long end = System.currentTimeMillis();
                     try {
-                        Thread.sleep(SCREENSHOT_INTERVAL - (end - start));
+                        long newTimeout = SCREENSHOT_INTERVAL - (end - start);
+                        Log.d("test", "timeout:" + newTimeout);
+                        if (newTimeout > 0){
+                            Thread.sleep(newTimeout);
+                        }
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
@@ -56,23 +63,31 @@ public class Worker {
     }
 
     private void takeScreenshot() {
+        AccessibilityNodeInfo root = mUiAutomation.getRootInActiveWindow();
         addBitmap(mUiAutomation.takeScreenshot());
     }
 
     private void addBitmap(Bitmap b) {
         mBitmaps.add(b);
-        if (mBitmaps.size() > IMAGETS_LIMIT) {
-            mBitmaps.remove(0).recycle();
+        if (mBitmaps.size() > IMAGETS_LIMIT - 1) {
+            Bitmap bitm = mBitmaps.remove(0);
+            if (!mNeededBitmaps.contains(bitm)) {
+                bitm.recycle();
+            }
         }
     }
 
-    public Bitmap[] getBitmaps() {
-        synchronized (mBitmaps) {
-            Bitmap[] result = new Bitmap[IMAGETS_LIMIT];
-            mBitmaps.toArray(result);
-            mBitmaps.clear();
-            return result;
+    public List<Bitmap> getBitmaps() {
+        for (Bitmap mNeededBitmap : mNeededBitmaps) {
+            mNeededBitmap.recycle();
         }
+        mNeededBitmaps.clear();
+        synchronized (mBitmaps) {
+            mNeededBitmaps.addAll(mBitmaps);
+            //mBitmaps.clear();
+        }
+        return mNeededBitmaps;
+
     }
 
 //    public List<Double> calculate(List<Double> input) {
